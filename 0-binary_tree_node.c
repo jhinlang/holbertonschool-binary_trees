b847d9ac34024bdff25fede8ef0c4cc0bd86babe
Julien <julien.hinlang@holbertonstudents.com>
2025-12-08 14:18:17 +0100

    feat: creating my file to make a new node of a binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include 
+/**
+ * binary_tree_node - is a function to creates a binary tree node
+ * @binary_tree_t: structure of binary tree node
+ * @parent: is a pointer to the parent node
+ * @value: is the value of the node
+ *
+ * Return : nothing
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+

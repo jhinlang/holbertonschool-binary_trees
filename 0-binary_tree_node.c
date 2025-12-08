9a3447df0437d7051ebce74a1c4e2dcd11b3ad0c
Julien <julien.hinlang@holbertonstudents.com>
2025-12-08 16:15:05 +0100

    fix: correcting syntax to answer betty's style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 /**
  * binary_tree_node - is a function to creates a binary tree node
- * @binary_tree_t: structure of binary tree node
  * @parent: is a pointer to the parent node
  * @value: is the value of the node
  *
- * Return : nothing
+ * Return: nothing
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {

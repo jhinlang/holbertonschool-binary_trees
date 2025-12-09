b572fe2cb37662e717436c135d6328591b3b6d6b
Julien <julien.hinlang@holbertonstudents.com>
2025-12-09 09:30:44 +0100

    feat: creating file 1 to task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - function thats inserts a node as the left
+ * @parent: is a pointer to the node parent
+ * @value: is the value of the node
+ *
+ * Return: nothing
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+

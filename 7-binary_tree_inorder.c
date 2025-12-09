542885cab5f2d64a406466a7d48375d69415d7f7
Vesna David ROSET <snaboy18@gmail.com>
2025-12-09 14:00:53 +0000

    feat(btrees): add binary_tree_inorder (in-order traversal)

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -14,16 +14,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
     {
         return;
     }
-
-    if (tree->left != NULL)
-    {
-        binary_tree_inorder( tree->left, func );
-    }
-
+    binary_tree_inorder(tree->left, func);
     func(tree->n);
+    binary_tree_inorder(tree->right, func);
 
-    if (tree->right != NULL)
-    {
-        binary_tree_inorder(tree->right, func);
-    }
 }

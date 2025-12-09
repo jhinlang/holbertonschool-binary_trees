6b1f1f9f893452a15bd932156b5dd3477d81bfe1
Vesna David ROSET <snaboy18@gmail.com>
2025-12-09 20:34:38 +0000

    feat(btrees): add binary_tree_balance (factor left−right)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,12 @@
 #include "binary_trees.h"
 #include "9-binary_tree_height.c"
 
+/**
+* binary_tree_balance - measures the balance factor of a binary tree
+* @tree: pointer to the root node of the tree to measure the balance factor
+*
+* Return: balance factor of the tree
+*/
 
 int binary_tree_balance(const binary_tree_t *tree)
 {

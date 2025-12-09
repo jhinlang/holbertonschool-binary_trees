572b3b27cb2c6f43cb72e797c118220bfcd0a54a
Vesna David ROSET <snaboy18@gmail.com>
2025-12-09 20:19:22 +0000

    feat(btrees): add binary_tree_balance (factor left−right)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,24 +2,19 @@
 
 /**
  * binary_tree_balance - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
+ * @tree: pointer to the root node of the tree to measure the balance
+ * factor
  *
- * Return: height of the tree
+ * Return: The balance factor either +ve, -ve or 0
+ * +ve if the height on the left > height on the right
+ * -ve if the height on the left < height on the right
+ *  0 if they are on the same level or tree is NULL
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0;
-	int right_height = 0;
-
 	if (tree == NULL)
-	return(0);
+	return (0);
 
-	if (tree->left != NULL)
-	left_height = binary_tree_balance(tree->left);
-
-	if (tree->right != NULL)
-	right_height = binary_tree_balance(tree->right);
-
-	return (left_height - right_height);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }

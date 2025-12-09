0cecd67899bb3b7f1a6d3162df91fb0e45d1bdf0
Julien <julien.hinlang@holbertonstudents.com>
2025-12-09 11:32:57 +0100

    fix: correcting syntax

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,6 +17,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new = malloc(sizeof(binary_tree_t));
 
+	if (new == NULL)
+	{
+		return (NULL)
+	}
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
 	if (parent->right != NULL)
 	{
 		new->right = parent->right;

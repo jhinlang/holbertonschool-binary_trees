ae19c6c60583ff50a08bbd8b1b5e362f7d8b8b9d
Julien <julien.hinlang@holbertonstudents.com>
2025-12-10 10:38:37 +0100

    feat: writing a function to check if a node has siblings

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - a function thats find the sibling of a node
+ * @node: is a pointer to the node to find sibling
+ *
+ * Return: nothing
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL)
+	}
+
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+
+	return (node->parent->left);
+}
